7cee7028b144f1bf91f606aa023819c772334a63
Cristina SJ <sjcristina9@gmail.com>
2024-08-28 17:51:09 -0300

    borda na statusbar

--- compiladores/src/main/java/com/pilador/view/StatusBar.java
+++ compiladores/src/main/java/com/pilador/view/StatusBar.java
@@ -3,12 +3,14 @@ package com.pilador.view;
 import java.awt.Dimension;
 
 import javax.swing.JLabel;
+import javax.swing.border.EmptyBorder;
 
 public class StatusBar extends JLabel {
 
     public StatusBar() {
         setPreferredSize(new Dimension(900, 25));
         setMinimumSize(new Dimension(900, 25));
+        setBorder(new EmptyBorder(0,10,0,0));
     }
 
     public void setStatusBarText(String text) {
